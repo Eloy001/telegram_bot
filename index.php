45c95d0844906adcab6ef4c719b520c46058ce63
Eloy <emm0001@alu.medac.es>
2022-02-08 20:31:22 +0100

    bot2

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ switch($message) {
         sendMessage($chatId, $response);
         break;
     case '/info':
-        $response = 'Hola! Soy @trecno_bot';
+        $response = 'Hola! Soy @xCrimson_bot';
         sendMessage($chatId, $response);
         break;
     default:
