7814514a6e7d218734e259bf473b0cf7ebefd806
Eloy <emm0001@alu.medac.es>
2022-02-17 16:39:02 +0100

    meteorologicos

--- index.php
+++ index.php
@@ -6,7 +6,13 @@ $update = json_decode($input, TRUE);
 $chatId = $update['message']['chat']['id'];
 $message = $update['message']['text'];
 $reply=$update["message"]["reply_to_message"]["text"];
-
+$urlDeportes="https://e00-elmundo.uecdn.es/blogs/elmundo/clima/index.xml";
+$urlInternacionales="https://www.abc.es/rss/feeds/abc_Internacional.xml";
+$urlEconomicas="https://e00-expansion.uecdn.es/rss/portada.xml";
+$urlCulturales="https://www.abc.es/rss/feeds/abc_ultima.xml";
+$urlAvisosEspaña="http://www.aemet.es/documentos_d/eltiempo/prediccion/avisos/rss/CAP_AFAE_wah_RSS.xml";
+$urlAvisosInternacionales="https://e00-elmundo.uecdn.es/blogs/elmundo/clima/index.xml";
+$urlNoticias="http://www.europapress.es/rss/rss.aspx";
     if(empty($reply)){
 
             switch($message) {
@@ -47,28 +53,28 @@ $reply=$update["message"]["reply_to_message"]["text"];
         if($reply=="¿Que tipo de noticia quieres ver? (deportes, generales, culturales, internacionales o economicas)"){
             switch ($message){
                 case 'deportes':
-                    getNoticiasDeportes($chatId);
+                    getNoticias($chatId, $urlDeportes);
                 break;
                 case 'general':
-                    getNoticias($chatId);
+                    getNoticias($chatId, $urlNoticias);
                 break;
                 case 'culturales':
-                    getNoticiasCulturales($chatId);
+                    getNoticias($chatId, $urlCulturales);
                 break;
                 case 'internacionales':
-                    getNoticiasInternacionales($chatId);
+                    getNoticias($chatId, $urlInternacionales);
                 break;
                 case 'economicas':
-                    getNoticiasEconomicas($chatId);
+                    getNoticias($chatId, $urlEconomicas);
                 break;
             }
         }if($reply=="¿Sobre cual pais quieres saber? (españa, internacionales)"){
             switch ($message){
                 case 'españa':
-                    getAvisosEspaña($chatId);
+                    getNoticias($chatId, $urlAvisosEspaña);
                 break;
                 case 'internacionales':
-                    getAvisosInternacionales($chatId);
+                    getNoticias($chatId, $urlAvisosInternacionales);
                 break;
         }
     }
@@ -77,94 +83,20 @@ function sendMessage($chatId, $response, $reply_markup="") {
     $url = $GLOBALS['website'].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.urlencode($response)."&reply_markup=".$reply_markup;
     file_get_contents($url);
 }
-function getNoticiasDeportes($chatId){
-    $context = stream_context_create(array('http' =>  array('header' => 'Accept: application/xml')));
-    $url = "https://e00-marca.uecdn.es/rss/futbol/primera-division.xml";
-    $xmlstring = file_get_contents($url, false, $context);
-    $xml = simplexml_load_string($xmlstring, "SimpleXMLElement", LIBXML_NOCDATA);
-    $json = json_encode($xml);
-    $array = json_decode($json, TRUE);
-    for ($i=0; $i < 9; $i++) { 
-    $titulos = $titulos."\n\n".$array['channel']['item'][$i]['title']."<a href='".$array['channel']['item'][$i]['link']."'> +info</a>";
-    }
-    sendMessage($chatId, $titulos);
-}
-function getNoticiasInternacionales($chatId){
-    $context = stream_context_create(array('http' =>  array('header' => 'Accept: application/xml')));
-    $url = "https://www.abc.es/rss/feeds/abc_Internacional.xml";
-    $xmlstring = file_get_contents($url, false, $context);
-    $xml = simplexml_load_string($xmlstring, "SimpleXMLElement", LIBXML_NOCDATA);
-    $json = json_encode($xml);
-    $array = json_decode($json, TRUE);
-    for ($i=0; $i < 9; $i++) { 
-    $titulos = $titulos."\n\n".$array['channel']['item'][$i]['title']."<a href='".$array['channel']['item'][$i]['link']."'> +info</a>";
-    }
-    sendMessage($chatId, $titulos);
-}
-function getNoticiasEconomicas($chatId){
-    $context = stream_context_create(array('http' =>  array('header' => 'Accept: application/xml')));
-    $url = "https://e00-expansion.uecdn.es/rss/portada.xml";
-    $xmlstring = file_get_contents($url, false, $context);
-    $xml = simplexml_load_string($xmlstring, "SimpleXMLElement", LIBXML_NOCDATA);
-    $json = json_encode($xml);
-    $array = json_decode($json, TRUE);
-    for ($i=0; $i < 9; $i++) { 
-    $titulos = $titulos."\n\n".$array['channel']['item'][$i]['title']."<a href='".$array['channel']['item'][$i]['link']."'> +info</a>";
-    }
-    sendMessage($chatId, $titulos);
-}
-
-function getNoticiasCulturales($chatId){
-    $context = stream_context_create(array('http' =>  array('header' => 'Accept: application/xml')));
-    $url = "https://www.abc.es/rss/feeds/abc_ultima.xml";
-    $xmlstring = file_get_contents($url, false, $context);
-    $xml = simplexml_load_string($xmlstring, "SimpleXMLElement", LIBXML_NOCDATA);
-    $json = json_encode($xml);
-    $array = json_decode($json, TRUE);
-    for ($i=0; $i < 9; $i++) { 
-    $titulos = $titulos."\n\n".$array['channel']['item'][$i]['title']."<a href='".$array['channel']['item'][$i]['link']."'> +info</a>";
-    }
-    sendMessage($chatId, $titulos);
-}
 
 function forzarRespuesta(){
     $reply_markup= array ('force_reply' => true, 'selective' => true);
     return json_encode($reply_markup, true);
 }
-function getNoticias($chatId){
+function getNoticias($chatId,$url){
     //include("simple_html_dom.php");
     $context = stream_context_create(array('http' =>  array('header' => 'Accept: application/xml')));
-    $url = "http://www.europapress.es/rss/rss.aspx";
     $xmlstring = file_get_contents($url, false, $context);
     $xml = simplexml_load_string($xmlstring, "SimpleXMLElement", LIBXML_NOCDATA);
     $json = j0son_encode($xml);
     $array = json_decode($json, TRUE);
     for ($i=0; $i < 9; $i++){ 
         $titulos = $titulos."\n\n".$array['channel']['item'][$i]['title']."<a href='".$array['channel']['item'][$i]['link']."'> +info</a>";
-    }
-    sendMessage($chatId, $titulos);
-}
-function getAvisosEspaña($chatId){
-    $context = stream_context_create(array('http' =>  array('header' => 'Accept: application/xml')));
-    $url = "http://www.aemet.es/documentos_d/eltiempo/prediccion/avisos/rss/CAP_AFAE_wah_RSS.xml";
-    $xmlstring = file_get_contents($url, false, $context);
-    $xml = simplexml_load_string($xmlstring, "SimpleXMLElement", LIBXML_NOCDATA);
-    $json = json_encode($xml);
-    $array = json_decode($json, TRUE);
-    for ($i=0; $i < 5; $i++) { 
-    $titulos = $titulos."\n\n".$array['channel']['item'][$i]['title']."<a href='".$array['channel']['item'][$i]['link']."'> +info</a>";
-    }
-    sendMessage($chatId, $titulos);
-}
-function getAvisosInternacionales($chatId){
-    $context = stream_context_create(array('http' =>  array('header' => 'Accept: application/xml')));
-    $url = "https://e00-elmundo.uecdn.es/blogs/elmundo/clima/index.xml";
-    $xmlstring = file_get_contents($url, false, $context);
-    $xml = simplexml_load_string($xmlstring, "SimpleXMLElement", LIBXML_NOCDATA);
-    $json = json_encode($xml);
-    $array = json_decode($json, TRUE);
-    for ($i=0; $i < 5; $i++) { 
-    $titulos = $titulos."\n\n".$array['channel']['item'][$i]['title']."<a href='".$array['channel']['item'][$i]['link']."'> +info</a>";
     }
     sendMessage($chatId, $titulos);
 }
